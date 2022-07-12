7066d46754b8020cd818ccfaa2778353535b1e9d
João Vitor Nunes Lobato de Souza <84386921+Jo40519@users.noreply.github.com>
2022-07-12 13:53:31 -0300

    ok

--- AngularSpotifyClone/src/app/pages/login/login.module.ts
+++ AngularSpotifyClone/src/app/pages/login/login.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { loginRotas } from './login.routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(loginRotas)
+    RouterModule.forChild(loginRotas),
+    ReactiveFormsModule
   ],
   declarations: [
     LoginComponent
